69ccca40b97d357d4e23acbae36f4cb78a73afd3
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-09-22 22:37:37 -0400

    add rpg landscapes image assets AND install Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      <h1>RPG</h1>
+      <h1 className="text-3xl font-bold underline text-center p-2">
+        Nameless RPG
+      </h1>
+      <img className="size-max" src="images/scotland3.webp" />
+      <div className="w-full h-screen bg-indigo-500"></div>
+      {/* <div className="w-100vw h-100vh bg-cover bg-center" style={{backgroundImage: "url('/images/scotland3.webp')"}}></div> */}
     </>
   )
 }
